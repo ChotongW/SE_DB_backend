eb5841e75c8de39f72888f5527a831621d4dd7b7
Wasin Wasantivong <chotongza@gmail.com>
2022-09-28 03:50:44 +0700

    initial basic backend and create get api

--- index.js
+++ index.js
@@ -10,17 +10,39 @@ app.use(bodyParser.urlencoded({
     extended: false
  }));
 
- /*
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "",
-    database: "carsystem"
+    database: "carleasing",
 })
-*/
 
 app.get('/', (req, res) => {
     res.send("hello world");
+});
+
+app.get('/vehicles', (req, res) => { 
+    db.query('SELECT * FROM vehicles', (err, result) =>{
+        if (err) throw err;
+        //console.log(rows);
+        res.send((result));
+        });
+});
+
+app.get('/vehicle/', (req, res) => { 
+    let vehicle_id = req.query.vehicleId;
+    /*
+    if (vehicle_id == null){
+        
+    }
+    */
+    //console.log(vehicle_id);
+    var sql = "SELECT * FROM vehicles WHERE vehicle_id = ?";
+    db.query(sql, vehicle_id, function(err, result, fields) { 
+    if (err) throw err;
+    //console.log(rows);
+    res.send((result));
+    });
 });
 
 app.listen('5500', () => {
