91caaa3f4eb68b2e00adc0eafa5b7f0cfaf0371d
Wasin Wasantivong <chotongza@gmail.com>
2022-10-06 11:44:27 +0700

    add async/await to blob upload add test deleted after upload

--- routes/payment.js
+++ routes/payment.js
@@ -14,6 +14,34 @@ router.use(
   })
 );
 
+router.get("/admin", userMiddleware.isLoggedIn, (req, res) => {
+  //let vehicle_id = req.query.vehicleId;
+  /*
+    if (vehicle_id == null){
+        
+    }
+    */
+  //console.log(vehicle_id);
+  var sql = "SELECT * FROM billing";
+  db.query(sql, (err, result) => {
+    if (err) throw (err, res.status(500).send(err));
+    //console.log(rows);
+    res.send(result);
+  });
+
+  // router.get("/vehicle/", cors(), function(req, res) {
+  //     let vehicle_id = req.query.vehicleId;
+  //     let name = req.query.name;
+
+  //     var sql = "SELECT * FROM vehicles WHERE vehicle_id = ? AND name >= ?";
+  //     db.query(sql, [ vehicle_id, name ], function(err, rows, fields) {
+  //     if (err) throw err;
+  //     //console.log(rows);
+  //     res.send((result));
+  //     });
+  //   });
+});
+
 router.post(
   "/",
   userMiddleware.isLoggedIn,
--- routes/test.js
+++ routes/test.js
@@ -4,6 +4,7 @@ const db = require("../config/db");
 const upload = require("../storage/multer");
 const blob = require("../storage/blobCar");
 const userMiddleware = require("../middleware/user");
+const fs = require("fs");
 
 router.get("/getAll", userMiddleware.isLoggedIn, (req, res) => {
   db.query("SELECT * FROM vehicles", (err, result) => {
@@ -13,18 +14,23 @@ router.get("/getAll", userMiddleware.isLoggedIn, (req, res) => {
   });
 });
 
-router.put("/upload", upload.single("file"), (req, res) => {
+router.post("/upload", upload.single("file"), async (req, res) => {
   let simpleFile = req.file;
 
   //upload to storage account
   try {
-    var callback = blob.blob_upload(simpleFile);
+    var callback = await blob.blob_upload(simpleFile);
     //res.redirect('/');
     console.log(callback);
     res.send("File uploaded successfully");
+    fs.unlink(simpleFile.path, (err) => {
+      if (err) throw err;
+      // if no error, file has been deleted successfully
+      console.log("File deleted!");
+    });
   } catch (error) {
     console.log(error);
-    res.status(500).send("Failure uploading");
+    //res.status(500).send("Failure uploading");
   }
   //console.log(upload_res);
   //   fs.unlink(simpleFile.path, (err) => {
