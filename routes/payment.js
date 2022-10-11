9215b53e29741d7ccb97961de6656031f1d0c45f
Wasin Wasantivong <chotongza@gmail.com>
2022-10-11 20:00:57 +0700

    refactor payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -71,22 +71,21 @@ router.put(
       // if no error, file has been deleted successfully
       console.log("Local file deleted!");
     });
-    var sql = "UPDATE billing \
-    SET slip =  ? \
-    WHERE bill_id = ?;";
-    db.query(sql, [callback, bill_id], (err, result) => {
-      if (!err) {
-        res.send(201, { response: "upload slip already" });
-        //res.redirect(201, '/');
-      } else {
+    queryDB(
+      "UPDATE billing SET slip =  ? WHERE bill_id = ?;",
+      [callback, bill_id],
+      (err) => {
         console.log(err);
         res.send(500, { response: err });
+      },
+      () => {
+        res.send(201, { response: "upload slip already" });
       }
-    });
+    );
   }
 );
 
-router.put("/admin/approve", userMiddleware.isLoggedIn, (req, res) => {
+router.put("/admin/approve", userMiddleware.isAdmin, (req, res) => {
   //ส่ง bill_id กลับมาด้วยนะ *** เป็น form-data นะ ***
   let bill_id = req.body.bill_id;
   //let booking_status = req.booking_status;
@@ -96,18 +95,17 @@ router.put("/admin/approve", userMiddleware.isLoggedIn, (req, res) => {
       message: "No bill found or bill id is null.",
     });
   }
-  var sql = "UPDATE billing \
-    SET bill_status =  ? \
-    WHERE bill_id = ?;";
-  db.query(sql, ["complete", bill_id], (err, result) => {
-    if (!err) {
-      res.send(201, { response: "bill update already" });
-      //res.redirect(201, '/');
-    } else {
+  queryDB(
+    "UPDATE billing SET bill_status =  ? WHERE bill_id = ?;",
+    ["complete", bill_id],
+    (err) => {
       console.log(err);
       res.send(500, { response: err });
+    },
+    () => {
+      res.send(201, { response: "bill update already" });
     }
-  });
+  );
 });
 
 function createBill(cost, id_no) {
@@ -116,35 +114,28 @@ function createBill(cost, id_no) {
   var id = uuid.v4();
   var tax_amount = cost * 0.07;
   var total_amount = cost + tax_amount;
-  var sql =
-    "INSERT INTO billing (bill_id, bill_status, amount_balance, total_amount, tax_amount) VALUES (?, ?, ?, ?, ?)";
-  db.query(
-    sql,
+  queryDB(
+    "INSERT INTO billing (bill_id, bill_status, amount_balance, total_amount, tax_amount) VALUES (?, ?, ?, ?, ?)",
     [id, "pending", cost, total_amount, tax_amount],
-    (err, result) => {
-      if (!err) {
-        return { message: "billed already" };
-        //res.redirect(201, '/');
-      } else {
-        console.log(err);
-        return { message: err };
-      }
+    (err) => {
+      console.log(err);
+      return { message: err };
+    },
+    () => {
+      return { message: "billed already" };
     }
   );
 
-  var sql = "UPDATE customer \
-    SET bill_id = ? \
-    WHERE id_no = ?;";
-  db.query(sql, [id, id_no], (err, result) => {
-    if (!err) {
-      console.log({ message: "update customer bill_id already" });
-      //res.send(201, { message: "booked already" });
-      //res.redirect(201, '/');
-    } else {
+  queryDB(
+    "UPDATE customer SET bill_id = ? WHERE id_no = ?;",
+    [id, id_no],
+    (err) => {
       console.log(err);
-      //res.send(500, err);
+    },
+    () => {
+      console.log({ message: "update customer bill_id already" });
     }
-  });
+  );
 }
 
 module.exports = {
